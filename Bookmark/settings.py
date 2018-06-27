4430240799a0b67d7f196cf256b2a2a1676d9829
Cristina Laryea <crispad06@gmail.com>
2018-06-27 03:33:36 -0400

    added category, personalbookmark, and fixed error in settings

--- Bookmark/settings.py
+++ Bookmark/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bookmarks',
+    'Bookmarks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
